6c00301b12dc360f9403632818c5ea38d9980f8e
Zhouchuan Huang <huangz@nju.edu.cn>
2024-11-14 15:42:04 +0800

    infor

--- src/sidebar data.jl
+++ src/sidebar data.jl
@@ -12,8 +12,7 @@ Dict(
             (name = "Z. Huang", url = "https://es.nju.edu.cn/hzc_en/list.htm")
         ],
         :title => "Fundamentals of Geophysics",
-        :subtitle => "For undergraduates",
-        :term => "Spring 2025",
+        :subtitle => "Spring 2025",
         :institution => "Nanjing University",
         :institution_url => "https://www.nju.edu.cn",
         :institution_logo => "julia-logo-color.svg",
