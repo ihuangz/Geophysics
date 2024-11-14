91112579acb2174cc41a170a5112c32f3ffce2e3
Zhouchuan Huang <huangz@nju.edu.cn>
2024-11-14 14:41:08 +0800

    Update sidebar data.jl

--- src/sidebar data.jl
+++ src/sidebar data.jl
@@ -3,7 +3,7 @@ Dict(
         "welcome" => collections["welcome"].pages,
         "Preliminaries" => collections["preliminaries"].pages,
         "Module 2: Social Science & Data Science" => collections["module2"].pages,
-        "Module 3: Climate Science" => collections["module3"].pages,
+        #"Module 3: Climate Science" => collections["module3"].pages,
 
     ],
     :about => Dict(
